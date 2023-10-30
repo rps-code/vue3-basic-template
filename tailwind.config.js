ca9276b7647b74c2ad7c4d156a3cdfec4b81cd0c
Karl Chelton <karl@absolute.agency>
2023-10-30 11:42:25 +0000

    🔧 Remove darkMode from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
             fontFamily: {
